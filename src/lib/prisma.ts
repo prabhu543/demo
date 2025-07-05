45954d79fa6c9a77ced36281ae2a4bd07b42e15c
Prabhu 543 <prabhuprathamesh444@gmail.com>
2025-07-05 09:22:58 +0530

    host

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,18 @@
+// import { PrismaClient } from "@prisma/client";
+
 import { PrismaClient } from '@/generated/prisma';
 
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client/edge";
+
+declare global {
+	// allow global `var` to be reused
+	var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+	global.prisma = prisma;
+}
+
 export default prisma;
